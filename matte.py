8fe139372749938fe34337787453abb5e836c7df
Robert Kolodziejczyk <robert.kolodziejczyk@ga.ntig.se>
2022-09-08 12:37:15 +0200

    frågesport

--- matte.py
+++ matte.py
@@ -6,3 +6,9 @@ månader = int(input("Månader:"))
 sec = 60*60*24*365*år + 60*60*24*30*månader
 
 print(f"Du är {sec} sekunder gammal")
+ 
+
+
+x = int(input("vilket tal vill du kvadrera?"))
+print(x*x)
+
