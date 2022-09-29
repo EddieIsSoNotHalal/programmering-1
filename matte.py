07114127d6cb454fe7b9c0194b62187f6fc9ed66
Robert Kolodziejczyk <robert.kolodziejczyk@ga.ntig.se>
2022-09-29 13:28:30 +0200

    hej

--- matte.py
+++ matte.py
@@ -1,4 +1,7 @@
 
+from ctypes.wintypes import WORD
+
+
 print("Hur gammal är du?")
 år = int(input("År:"))
 månader = int(input("Månader:"))
@@ -7,8 +10,5 @@ sec = 60*60*24*365*år + 60*60*24*30*månader
 
 print(f"Du är {sec} sekunder gammal")
  
-
-
 x = int(input("vilket tal vill du kvadrera?"))
 print(x*x)
-
